918326547f72ae763ff89bfbded9ba280c5148eb
hstehouwer <hstehouwer@jibes.nl>
2020-12-01 15:04:59 +0100

    much faster

--- day1.py
+++ day1.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3.8
 import timeit
 
-from readers.read_lists import read_list_ints
+from readers.read_lists import read_list_ints_sorted
 from core.findsum import find_inputs_that_sumto
 import math
 
 
 def both(number_of_inputs:int = 2):
-    with read_list_ints("data/day1_input.txt") as intlist:
+    with read_list_ints_sorted("data/day1_input.txt") as intlist:
         result = find_inputs_that_sumto(intlist, 2020, number_of_inputs)
         return result, math.prod(result)
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     print("Day1_Part2: found tuple: {} that multiplies to: {}".format(result[0], result[1]))
 
     # timing part2 without IO
-    with read_list_ints("data/day1_input_boudewijn.txt") as intlist:
+    with read_list_ints_sorted("data/day1_input_boudewijn.txt") as intlist:
         iterations = 100
         t = timeit.Timer(stmt='find_inputs_that_sumto(intlist, 2020, 3)', globals=globals())
         performance_result_ms = t.timeit(number=iterations) / iterations * 1000
--- readers/read_lists.py
+++ readers/read_lists.py
@@ -2,8 +2,9 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def read_list_ints(filename:str):
+def read_list_ints_sorted(filename:str):
     with open(filename) as f:
         data = f.readlines()
     data = [int(x) for x in data]
+    data.sort()
     yield data
