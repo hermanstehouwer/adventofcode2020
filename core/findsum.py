d51c68e1b989936447f598e88807fea9faa60611
hstehouwer <hstehouwer@jibes.nl>
2020-12-01 08:48:14 +0100

    small improvements

--- core/findsum.py
+++ core/findsum.py
@@ -3,7 +3,7 @@ import itertools
 
 
 def find_inputs_that_sumto(input: List[int], target_sum: int = 2020, number_of_inputs:int = 2) -> List[int]:
-    for p in itertools.permutations(input, number_of_inputs):
+    for p in itertools.combinations(input, number_of_inputs):
         if sum(p) == target_sum:
-            return [x for x in p]
+            return p
     raise ValueError(f"Cannot find two elements in the list that sum to {target_sum}")
--- test/test_day1.py
+++ test/test_day1.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import fixture
 
 from core import findsum
@@ -7,16 +8,16 @@ def short_test_list():
     l = [1721,979,366,299,675,1456]
     yield l
 
+@pytest.mark.parametrize("num_to_find, expected", [
+    pytest.param(2, [1721, 299]),
+    pytest.param(3, [979, 366, 675])
+])
+def test_day1_positive(short_test_list, num_to_find, expected):
+    to_verify = findsum.find_inputs_that_sumto(short_test_list, 2020, num_to_find)
+    for ev in expected:
+        assert ev in to_verify
 
-def test_day1_part1(short_test_list):
-    to_verify = findsum.find_inputs_that_sumto(short_test_list, 2020)
-    assert 1721 in to_verify
-    assert 299 in to_verify
 
-
-def test_day1_part2(short_test_list):
-    to_verify = findsum.find_inputs_that_sumto(short_test_list, 2020, 3)
-    assert 979 in to_verify
-    assert 366 in to_verify
-    assert 675 in to_verify
-
+def test_day1_valueerror(short_test_list):
+    with pytest.raises(ValueError):
+        findsum.find_inputs_that_sumto(short_test_list, 0)
