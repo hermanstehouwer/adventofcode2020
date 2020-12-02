3d1eda44eb6f06e994de775cd1e8c7465fbc93b6
hstehouwer <hstehouwer@jibes.nl>
2020-12-02 10:30:23 +0100

    slight refactor

--- test/test_day2.py
+++ test/test_day2.py
@@ -1,16 +1,14 @@
 import pytest
-from core.passwords_and_policy import SledRentalPolicy, OfficialTobogganPolicy
+
+from core.passwords_and_policy import SledRentalPolicy, OfficialTobogganPolicy, PasswordPolicy
 
 
-@pytest.mark.parametrize("password_line, expected_outcome, PolicyClass", [
-    pytest.param("1-3 a: abcde", True, SledRentalPolicy),
-    pytest.param("1-3 b: cdefg", False, SledRentalPolicy),
-    pytest.param("2-9 c: ccccccccc", True, SledRentalPolicy),
-    pytest.param("1-3 a: abcde", True, OfficialTobogganPolicy),
-    pytest.param("1-3 b: cdefg", False, OfficialTobogganPolicy),
-    pytest.param("2-9 c: ccccccccc", False, OfficialTobogganPolicy)
+@pytest.mark.parametrize("password_line, expected_outcome_sled, expected_outcome_toboggan", [
+    pytest.param("1-3 a: abcde", True, True),
+    pytest.param("1-3 b: cdefg", False, False),
+    pytest.param("2-9 c: ccccccccc", True, False)
 ])
-def test_password_lines(password_line, expected_outcome, PolicyClass):
+def test_password_lines(password_line, expected_outcome_sled, expected_outcome_toboggan):
     policy_str, password = password_line.split(": ")
-    p = PolicyClass(policy_str)
-    assert p.check_password_against_policy(password) == expected_outcome
+    assert SledRentalPolicy(policy_str).check_password_against_policy(password) == expected_outcome_sled
+    assert OfficialTobogganPolicy(policy_str).check_password_against_policy(password) == expected_outcome_toboggan
