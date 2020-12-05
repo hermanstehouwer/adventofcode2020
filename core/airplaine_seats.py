09be9d910159b0931f76d09898142f9617cdcb35
hstehouwer <hstehouwer@jibes.nl>
2020-12-05 07:09:02 +0100

    small refactor

--- core/airplaine_seats.py
+++ core/airplaine_seats.py
@@ -3,14 +3,10 @@ class Seat:
     column: int
 
     def __init__(self, boarding_pass):
-        # row
-        row_desc = boarding_pass[0:-3]
-        row_desc = row_desc.replace("F", "0").replace("B", "1")
+        row_desc = boarding_pass[0:-3].translate(str.maketrans("FB", "01"))
         self.row = int(row_desc, 2)
 
-        # column
-        column_desc = boarding_pass[-3:]
-        column_desc = column_desc.replace("L", "0").replace("R", "1")
+        column_desc = boarding_pass[-3:].translate(str.maketrans("LR", "01"))
         self.column = int(column_desc, 2)
 
     def get_ID(self):
